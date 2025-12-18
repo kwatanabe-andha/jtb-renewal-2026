c2e3e1898fbcf2b294707e730329eb8a3bd2c3a4
kwatanabe-andha <k_watanabe@and-ha.com>
2025-12-18 19:22:02 +0900

    APIテスト（API Url 調整）

--- src/fetch/getColumns.ts
+++ src/fetch/getColumns.ts
@@ -1,6 +1,7 @@
 export default async function getColumns() {
   const url = new URL(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/3/columns/list`,
+    // `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/3/columns/list`,
+    `https://tourism.g.kuroco.app/rcms-api/3/columns/list`,
   )
   const response = await fetch(url)
   return await response.json()

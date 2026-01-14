8442f73bcd04f46bcb3d6d046dec023d0271dcfc
dk-watanabe <watanabe@ds-k.site>
2026-01-14 11:39:58 +0900

    ヘッダー、regionTop のサイズ調整

---
+++ out/dev/static/chunks/src_app_insights_series_page_tsx_3eebdf9f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ff8e282b._.js",
+  "static/chunks/src_6c9e03e5._.js",
+  "static/chunks/src_components_e50622ef._.css"
+],
+    source: "dynamic"
+});

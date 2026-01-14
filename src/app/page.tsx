3a3a84ef935e9284954acb1c954f8e776d5b6cfd
dk-watanabe <watanabe@ds-k.site>
2026-01-14 11:44:32 +0900

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import '@/styles/index.scss';
 
 export default async function Home() {
-  const url = new URL(
-    `http://localhost:10028/wp-json/wp/v2/column?page=1`,
-  )
-  const response = await fetch(url, {credentials: 'omit'})
-  const data = await response.json()
-  const totalPages = await response.headers.get('x-wp-totalpages')
-  console.log(data)
-  console.log(totalPages)
+  // const url = new URL(
+  //   `http://localhost:10028/wp-json/wp/v2/column?page=1`,
+  // )
+  // const response = await fetch(url, {credentials: 'omit'})
+  // const data = await response.json()
+  // const totalPages = await response.headers.get('x-wp-totalpages')
+  // console.log(data)
+  // console.log(totalPages)
 
   return (
     <div className=''>

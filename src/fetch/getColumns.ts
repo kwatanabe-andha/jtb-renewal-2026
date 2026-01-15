238dc9d51d16688004b6808e890c5cb8f1ee080c
dk-watanabe <watanabe@ds-k.site>
2026-01-15 21:47:44 +0900

    build用 調整

--- src/fetch/getColumns.ts
+++ src/fetch/getColumns.ts
@@ -5,17 +5,23 @@ type PropsType = {
 export default async function getColumns(params?: PropsType) {
   const { pageID = '1' } = params || {}
 
+  // const url = new URL(
+  //   `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/7/insights/list`,
+  // )
+  // url.searchParams.append('pageID', String(pageID))
+  
+  // const response = await fetch(url,
+  //   {
+  //     headers: {
+  //       'X-RCMS-API-ACCESS-TOKEN': process.env.INSIGHTS_ACCESS_TOKEN!,
+  //     }
+  //   }
+  // )
   const url = new URL(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/rcms-api/7/insights/list`,
+    `https://tourism.g.kuroco.app/rcms-api/7/insights/list`,
   )
   url.searchParams.append('pageID', String(pageID))
   
-  const response = await fetch(url,
-    {
-      headers: {
-        'X-RCMS-API-ACCESS-TOKEN': process.env.INSIGHTS_ACCESS_TOKEN!,
-      }
-    }
-  )
+  const response = await fetch(url)
   return await response.json()
 }
